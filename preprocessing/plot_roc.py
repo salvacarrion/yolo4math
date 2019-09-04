5996281cbff4405b797fd44df5d1ba1351cd3023
salvacarrion <salva.carrion@outlook.com>
2019-09-04 11:05:32 +0200

    Add plots

--- preprocessing/plot_roc.py
+++ preprocessing/plot_roc.py
@@ -8,13 +8,8 @@ from utils.utils import load_dataset, load_obj
 from sklearn.metrics import roc_curve, auc
 from models.ssd.utils import find_jaccard_overlap
 
-#
-# json_path = "/Users/salvacarrion/Documents/Experiments/tfm/YOLOv3-1024-noweights-6clusters/YOLO_stats.json"
-# # json_path = "/Users/salvacarrion/Documents/Experiments/tfm/SSD-1024-1024-adam-alpha2.0/SSD_stats.json"
-# raw_data = load_dataset(json_path)
-#
 
-base_path = "/Users/salvacarrion/Documents/Programming/Python/yolo4math/models/yolov3"
+base_path = "/Users/salvacarrion/Documents/Programming/Python/yolo4math/models/ssd"
 predictions = load_obj(base_path + "/predictions.pkl")
 #confusion_matrix = load_obj(base_path + "/confusion_matrix.pkl")
 stats = load_dataset(base_path + "/stats.json")
@@ -99,37 +94,3 @@ plt.legend(loc="lower right")
 plt.show()
 
 asdasd =3
-
-#
-# data_embedded = {
-#     'recall': stats['classes']['0']['recall11'],
-#     'precision': stats['classes']['0']['precision11'],
-# }
-# data_isolated = {
-#     'recall': stats['classes']['1']['recall11'],
-#     'precision': stats['classes']['1']['precision11'],
-# }
-
-# Plots
-
-# ax2 = sns.lineplot(x='conf', y='precision', data=pd.DataFrame(data), markers=True,  label='Precision')
-#
-# # Settings
-# plt.figure()
-# ax1 = sns.lineplot(x='recall', y='precision', data=pd.DataFrame(data_embedded), markers=True, label='Embedded', drawstyle='steps-pre')
-# ax1 = sns.lineplot(x='recall', y='precision', data=pd.DataFrame(data_isolated), markers=True, label='Isolated', drawstyle='steps-pre')
-# # ax1 = sns.lineplot(data=pd.DataFrame(data, index='recall'), sort=False, legend="full", markers=True, )
-# # plt.title('ROC curve')
-# # plt.xlabel('False Positive Rate')
-# # plt.ylabel('True Positive Rate')
-# # plt.title('Recall-Precision curve')
-# # plt.xlabel('Recall')
-# # plt.ylabel('Precision')
-# plt.ylim([0.0, 1.05])
-# plt.xlim([0.0, 1.0])
-# plt.legend()
-#
-# # Show and save
-# plt.savefig('1024-rp.eps')
-# plt.show()
-# asdas = 3
